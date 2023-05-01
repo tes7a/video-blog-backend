2d9de718e30d4a8d1424e8ea244ead91091ffb49
Kostiantyn <ttes7a@gmail.com>
2023-05-01 13:45:29 +0300

    delete dist folder

--- dist/routes/testing.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Reset the database for testing
-videosRoute.delete("/all-date", (req, res) => {
-    if (videos) {
-        videos = [];
-        return res.sendStatus(201);
-    }
-    return res.sendStatus(404);
-});
