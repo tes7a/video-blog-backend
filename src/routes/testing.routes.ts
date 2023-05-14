1e7143491db52304ad54b8241a5d92f353538f18
Kostiantyn <ttes7a@gmail.com>
2023-05-14 12:37:36 +0300

    refactoring testing routs

--- src/routes/testing.routes.ts
+++ src/routes/testing.routes.ts
@@ -1,14 +1,14 @@
 import { Request, Response, Router } from "express";
 import { videos } from "../db/videos.db";
-import { blogs } from "../db/blogs.db";
-import { posts } from "../db/posts.db";
+
+import { blogsDb, postsDb } from "../db/db";
 
 export const testingRoute = Router({});
 
 // Reset the database for testing
-testingRoute.delete("/all-data", (req: Request, res: Response) => {
+testingRoute.delete("/all-data", async (req: Request, res: Response) => {
   videos.length = 0;
-  blogs.length = 0;
-  posts.length = 0;
+  await blogsDb.deleteMany({});
+  await postsDb.deleteMany({});
   return res.sendStatus(204);
 });
