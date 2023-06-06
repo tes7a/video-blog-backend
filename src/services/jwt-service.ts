8a92e3d4c308f99fafda346a16150dcd0ab908c3
Kostiantyn <ttes7a@gmail.com>
2023-06-06 19:33:28 +0300

    fixed

--- src/services/jwt-service.ts
+++ src/services/jwt-service.ts
@@ -6,7 +6,7 @@ import { log } from "console";
 export const jwtService = {
   async createJWT(user: UsersDbModel) {
     const token = jwt.sign({ userId: user.id }, settings.JWT_SECRET, {
-      expiresIn: "10sec",
+      expiresIn: "20sec",
     });
 
     return { accessToken: token };
