cdc05d03a87249881183ce4f29acce8deaadc2fb
Kostiantyn <ttes7a@gmail.com>
2023-07-13 22:18:42 +0300

    create middleware

--- src/middleware/api-connects-middleware.ts
+++ src/middleware/api-connects-middleware.ts
@@ -1,3 +1,33 @@
-export const apiConnectMiddleware = () => {
-  /// what logic need to add??
+import { NextFunction, Request, Response } from "express";
+
+interface RequestLog {
+  ip: string;
+  url: string;
+  date: Date;
+}
+
+const requestLogs: RequestLog[] = [];
+
+export const apiConnectMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { ip, url } = req;
+  const currentDate = new Date();
+
+  const filteredLogs = requestLogs.filter(
+    (log) =>
+      log.ip === ip &&
+      log.url === url &&
+      log.date >= new Date(currentDate.getTime() - 10000)
+  );
+
+  requestLogs.push({
+    ip,
+    url,
+    date: currentDate,
+  });
+
+  next();
 };
